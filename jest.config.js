0952d19253db538e521fa10b2ead6b2bdb774c72
Tim Vučina <56194319+vucinatim@users.noreply.github.com>
2023-08-21 14:18:52 +0200

    Feature/unit tests (#9)
    * feat: add unit testing
    
    * fix: added test:ci
    
    * fix: new github action
    
    * fix: gh action test on push
    
    * fix: improved unit testing structure
    
    * fix: removed on branch push gh action

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/*.test.[jt]s?(x)'],
+  moduleDirectories: ['node_modules', '<rootDir>'], // <-- This is important for Jest to pick up your external __mocks__ directory
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(config);
---
+++ src/components/sample-form.test.tsx
@@ -0,0 +1,61 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+import SampleForm from '@/components/sample-form';
+
+jest.mock('next-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+    i18n: {
+      language: 'en',
+    },
+  }),
+}));
+
+describe('SampleForm Component', () => {
+  it('renders correctly', () => {
+    render(<SampleForm />);
+
+    // Check that inputs and button are present
+    expect(screen.getByPlaceholderText('email')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('password')).toBeInTheDocument();
+    expect(screen.getByText('submit')).toBeInTheDocument();
+  });
+
+  it('captures input values and handles form submission', async () => {
+    render(<SampleForm />);
+
+    const emailInput = screen.getByPlaceholderText('email');
+    const passwordInput = screen.getByPlaceholderText('password');
+    const submitButton = screen.getByText('submit');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123' } });
+
+    fireEvent.click(submitButton);
+
+    // Assuming that submitting would make the inputs disabled
+    await waitFor(() => expect(emailInput).toBeDisabled());
+    await waitFor(() => expect(passwordInput).toBeDisabled());
+  });
+
+  it('shows validation errors', async () => {
+    render(<SampleForm />);
+
+    const emailInput = screen.getByPlaceholderText('email');
+    const passwordInput = screen.getByPlaceholderText('password');
+    const submitButton = screen.getByText('submit');
+
+    // Providing invalid values
+    fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
+    fireEvent.change(passwordInput, { target: { value: 'short' } });
+    fireEvent.click(submitButton);
+
+    // Check for validation error messages
+    await waitFor(() => {
+      const errorMessages = screen.getAllByText('this field is required');
+      return expect(errorMessages).toHaveLength(2);
+    });
+  });
+});
