34cd79eaea9fefc8646ccf2efbfee4f9971068a8
Žan Horvat <zanhorvat8@gmail.com>
2023-11-14 16:27:04 +0100

    fix: matcher in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,5 +14,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)'],
+  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt|_axiom).*)'],
 };
