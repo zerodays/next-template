cbc4c54043cdb5d509fb60df5740657fde5172b4
Domen Koščak <domen.koscak@zerodays.dev>
2023-12-04 11:55:06 +0100

    fix: lint errors

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import { Inter } from 'next/font/google';
 
 import SampleForm from '@/components/sample-form';
 import serverLogger from '@/lib/axiom/log-server';
+import { useScopedI18n } from '@/i18n/client';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Page() {
+  const t = useScopedI18n('common');
+
   const logger = serverLogger();
 
   logger.info('Hello from server logger!');
@@ -13,12 +18,8 @@ export default function Page() {
   return (
     <main
       className={`flex h-screen flex-col items-center justify-center gap-8 text-center ${inter.className}`}>
-      <h1 className="text-4xl font-bold">
-        Next.js + Tailwind CSS + TypeScript
-      </h1>
-      <h2 className="text-lg font-bold text-gray-500">
-        with i18n, jest, zod, react-hook-form
-      </h2>
+      <h1 className="text-4xl font-bold">{t('title')}</h1>
+      <h2 className="text-lg font-bold text-gray-500">{t('subtitle')}</h2>
       <SampleForm />
     </main>
   );
